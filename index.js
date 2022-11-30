df33e15da8507babe21c75253c5b0d38cea712c8
Oliver Maurhart <dyle71@gmail.com>
2022-11-30 21:13:00 +0100

    Adds first step of htl-villach theme

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ app.use('/reveal.js-plugin', express.static('node_modules/reveal.js/plugin'))
 app.use('/reveal.js-plugin-external', express.static('node_modules/reveal_external'))
 app.use('/reveal.js-plugin-menu', express.static('node_modules/reveal.js-menu'))
 app.use('/plugin', express.static('plugin'))
+app.use('/themes', express.static('themes'))
 
 app.use('/', express.static('slides'))
 app.listen(port, () => {
